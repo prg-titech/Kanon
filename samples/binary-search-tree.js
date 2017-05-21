06eddc1025782b4e14ce405524bc5846ce48e6bd
akiou <tenniswokya7117@gmail.com>
2017-05-21 17:39:45 +0900

    added rotate function (but it's not completed)

--- samples/binary-search-tree.js
+++ samples/binary-search-tree.js
@@ -34,6 +34,24 @@ BST.prototype.add = function(val) {
     }
 };
 
+BST.prototype.rotateRight = function() {
+    if (this.root === null || this.root.left === null) {
+        return;
+    } else {
+        var new_root = this.root.left;
+        if (new_root.right) {
+            new_root.right.parent = new_root.parent;
+            new_root.parent.left = new_root.right;
+            new_root.right = new_root.parent;
+            delete new_root.parent;
+            this.root.parent = new_root;
+            this.root = new_root;
+        } else {
+            // TODO
+        }
+    }
+};
+
 var tree = new BST();
 tree.add(5);
 tree.add(3);
@@ -42,3 +60,6 @@ tree.add(4);
 tree.add(6);
 tree.add(1);
 tree.add(10);
+
+tree.rotateRight();
+tree.rotateRight(); 
