a3bf3e4c62902997863d7008b0407042be92aceb
akiou <tenniswokya7117@gmail.com>
2017-05-25 01:11:18 +0900

    Enabled arrays to be drawn by using square and make demo directory.

--- src/js/context.js
+++ src/js/context.js
@@ -567,3 +567,13 @@ __$__.Context = {
         return nearestLoopLabels;
     }
 };
+
+
+__$__.Context.getObjectID = function(obj) {
+    let index = Object.values(__$__.Context.Objects).indexOf(obj);
+    if (index === -1) {
+        return undefined;
+    } else {
+        return Object.keys(__$__.Context.Objects)[index];
+    }
+};
