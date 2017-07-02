bb6cd28f2ae8fcaf19b525ab639a354f24ae9f6c
akiou <tenniswokya7117@gmail.com>
2017-07-02 23:43:46 +0900

    some modifications

--- samples/binary-search-tree.js
+++ samples/binary-search-tree.js
@@ -1,65 +1,131 @@
-function Node(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-};
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
+}
 
-function BST() {
-    this.root = null;
-};
+class BST {
+    constructor() {
+        this.root = null;
+    }
 
-BST.prototype.add = function(val) {
-    var temp = new Node(val);
-    
-    if (this.root === null) {
-        this.root = temp;
-    } else {
-        var current = this.root;
-        
-        while (current.val <= val && current.right || current.val > val && current.left) {
-            if (current.val <= val && current.right) {
-                current = current.right;
+    add(val) {
+        let temp = new Node(val);
+
+        if (this.root === null) {
+            this.root = temp;
+        } else {
+            let current = this.root;
+
+            while (current.val <= val && current.right || current.val > val && current.left) {
+                if (current.val <= val) {
+                    current = current.right;
+                } else {
+                    current = current.left;
+                }
+            }
+            if (current.val <= val) {
+                current.right = temp;
+                temp.parent = current;
             } else {
-                current = current.left;
+                current.left = temp;
+                temp.parent = current;
             }
         }
-        
-        if (current.val <= val) {
-            current.right = temp;
-        } else {
-            current.left = temp;
-        }
-        temp.parent = current;
     }
-};
 
-BST.prototype.rotateRight = function() {
-    if (this.root === null || this.root.left === null) {
-        return;
-    } else {
-        var new_root = this.root.left;
-        if (new_root.right) {
-            new_root.right.parent = new_root.parent;
-            new_root.parent.left = new_root.right;
-            new_root.right = new_root.parent;
+    rotateRight() {
+        if (this.root && this.root.left) {
+            let current = this.root.left;
+            let new_root = current;
             delete new_root.parent;
+            current = current.right;
+            this.root.left.right = this.root;
             this.root.parent = new_root;
+
+            this.root.left = current;
+            current.parent = this.root;
             this.root = new_root;
-        } else {
-            // TODO
         }
     }
-};
 
-var tree = new BST();
-tree.add(5);
-tree.add(3);
-tree.add(8);
-tree.add(4);
+    delete(val) {
+        if (this.root) {
+            let delete_node = this.root;
+            while (delete_node && delete_node.val !== val) {
+                if (delete_node.val < val)
+                    delete_node = delete_node.right;
+                else if (delete_node.val > val)
+                    delete_node = delete_node.left;
+            }
+            if (!delete_node)
+                return;
+
+            if (!delete_node.left && !delete_node.right) {
+                if (delete_node.parent.right === delete_node)
+                    delete delete_node.parent.right;
+                else
+                    delete delete_node.parent.left;
+                delete delete_node.parent;
+            } else if (!delete_node.left) {
+                if (delete_node.parent.right === delete_node) {
+                    // TODO
+                } else {
+                    delete_node.right.parent = delete_node.parent;
+                    delete_node.parent.left = delete_node.right;
+                    delete delete_node.right;
+                    delete delete_node.parent;
+                }
+            } else if (!delete_node.right) {
+                if (delete_node.parent.right === delete_node) {
+                    // TODO
+                } else {
+                    delete_node.parent.left = delete_node.left;
+                    delete_node.left.parent = delete_node.parent;
+                    delete delete_node.left;
+                    delete delete_node.parent;
+                }
+            } else {
+                let current = delete_node.left;
+
+                while (current.right)
+                    current = current.right;
+
+                if (current === delete_node.left) {
+                    // TODO
+                } else if (current.left) {
+                    // TODO
+                } else {
+                    delete current.parent.right;
+                    delete current.parent;
+                    current.left = delete_node.left;
+                    current.right = delete_node.right;
+                    current.left.parent = current;
+                    current.right.parent = current;
+                    delete delete_node.left;
+                    delete delete_node.right;
+                    this.root = current;
+                }
+            }
+        }
+    }
+
+    search(val) {
+        // TODO
+    }
+}
+
+let tree = new BST();
+
 tree.add(6);
+tree.add(4);
+tree.add(2);
+tree.add(3);
+tree.add(7);
+tree.add(5);
 tree.add(1);
-tree.add(10);
-
 tree.rotateRight();
-tree.rotateRight(); 
+tree.search(4);
--- samples/doubly-linked-list.js
+++ samples/doubly-linked-list.js
@@ -1,102 +1,108 @@
-let Node = function(val) {
-    this.val = val;
-    this.next = null;
-    this.prev = null;
-};
-
-let DLList = function() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-};
-
-DLList.prototype.add = function(val) {
-    let temp = new Node(val);
-    
-    if (!this.head) {
-        this.head = temp;
-        this.tail = temp;
-    } else {
-        temp.prev = this.tail;
-        this.tail.next = temp;
-        this.tail = temp;
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
     }
-    this.length++;
-};
+}
 
-DLList.prototype.insert = function(val, index) {
-    if (0 <= index && index <= this.length) {
+class DLList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    add(val) {
         let temp = new Node(val);
-        
-        if (index === this.length) {
+
+        if (!this.head) {
+            this.head = temp;
+            this.tail = temp;
+        } else {
             temp.prev = this.tail;
             this.tail.next = temp;
             this.tail = temp;
-        } else if (index === 0) {
-            temp.next = this.head;
-            this.head.prev = temp;
-            this.head = temp;
-        } else {
-            let current = this.head;
-            
-            for (var i = 0; i < index; i++) {
-                current = current.next;
-            }
-            temp.prev = current.prev;
-            temp.next = current;
-            current.prev.next = temp;
-            current.prev = temp;
         }
         this.length++;
     }
-};
 
-DLList.prototype.remove = function(index) {
-    if (0 <= index && index < this.length) {
-        if (this.length === 1) {
-            delete this.head;
-            delete this.tail;
-        } else if (0 === index) {
-            this.head = this.head.next;
-            delete this.head.prev.next;
-            delete this.head.prev;
-        } else if (index === this.length-1) {
-            this.tail = this.tail.prev;
-            delete this.tail.next.prev;
-            delete this.tail.next;
-        } else {
-            let current = this.head;
-            
-            for (var i = 0; i < index; i++) {
-                current = current.next;
+    insert(val, idx) {
+        if (0 <= idx && idx <= this.length) {
+            let temp = new Node(val);
+
+            if (idx === this.length) {
+                temp.prev = this.tail;
+                this.tail.next = temp;
+                this.tail = temp;
+            } else if (idx === 0) {
+                temp.next = this.head;
+                this.head.prev = temp;
+                this.head = temp;
+            } else {
+                let current = this.head;
+
+                for (let i = 0; i < idx; i++) {
+                    current = current.next;
+                }
+                temp.prev = current.prev;
+                temp.next = current;
+                current.prev.next = temp;
+                current.prev = temp;
             }
-            
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
-            delete current.prev;
-            delete current.next;
+            this.length++;
         }
-        this.length--;
     }
-};
 
-DLList.prototype.reverse = function() {
-    if (this.length > 0) {
-        let current = this.head, temp = current.next;
-        
-        while (current !== this.tail) {
-            current.prev = temp;
-            temp = temp.next;
-            current.prev.next = current;
-            if (current === this.head) delete current.next;
-            delete current.prev.prev;
-            current = current.prev;
+    remove(index) {
+        if (0 <= index && index < this.length) {
+            if (this.length === 1) {
+                delete this.head;
+                delete this.tail;
+            } else if (0 === index) {
+                this.head = this.head.next;
+                delete this.head.prev.next;
+                delete this.head.prev;
+            } else if (index === this.length-1) {
+                this.tail = this.tail.prev;
+                delete this.tail.next.prev;
+                delete this.tail.next;
+            } else {
+                let current = this.head;
+
+                for (let i = 0; i < index; i++) {
+                    current = current.next;
+                }
+
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                delete current.prev;
+                delete current.next;
+            }
+            this.length--;
         }
-        
-        this.tail = this.head;
-        this.head = current;
     }
-};
+
+    reverse() {
+        if (this.length > 0) {
+            let current = this.head, temp = current.next;
+
+            while (current !== this.tail) {
+                current.prev = temp;
+                temp = temp.next;
+                current.prev.next = current;
+                if (current === this.head) delete current.next;
+                delete current.prev.prev;
+                current = current.prev;
+            }
+
+            this.tail = this.head;
+            this.head = current;
+        }
+    }
+}
+
+
 
 let list = new DLList();
 
