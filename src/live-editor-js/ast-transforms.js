de2592c6755aa630d7963509fc3633db632a2cbf
akiou <tenniswokya7117@gmail.com>
2018-04-28 12:15:10 +0900

    remove the checkpoints after return statement

--- src/live-editor-js/ast-transforms.js
+++ src/live-editor-js/ast-transforms.js
@@ -1775,18 +1775,6 @@ __$__.ASTTransforms.InsertCheckPoint = function() {
                  * {
                  *     checkpoint;
                  *     let __temp = hoge;
-                 *     do {
-                 *         out_loop
-                 *     } while (__loopLabels.pop().indexOf('Statement') >= 0)
-                 *     __$__.Context.ChangedGraph = true;
-                 *     return __temp;
-                 *     checkpoint;
-                 * }
-                 *
-                 * // after
-                 * {
-                 *     checkpoint;
-                 *     let __temp = hoge;
                  *     __time_counter_stack.last().end = __time_counter-1;
                  *     __time_counter_stack.pop();
                  *     while (__loopLabels.pop().indexOf('Statement') >= 0) {
@@ -1794,7 +1782,6 @@ __$__.ASTTransforms.InsertCheckPoint = function() {
                  *     }
                  *     __$__.Context.ChangedGraph = true;
                  *     return __temp;
-                 *     checkpoint;
                  * }
                  */
                 if (node.type === 'ReturnStatement') {
@@ -1863,8 +1850,7 @@ __$__.ASTTransforms.InsertCheckPoint = function() {
                         changedGraphStmt(),
                         b.ReturnStatement(
                             b.Identifier('__temp')
-                        ),
-                        checkPoint(end, variables)
+                        )
                     ]);
                 /**
                  * // before
