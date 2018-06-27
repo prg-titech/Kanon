ce898ca51eb2a5ce0e9b3b2040cddf947d1b09e6
Rohan Ray Sobha <r.r.sobha@student.tudelft.nl>
2018-06-27 17:53:49 +0900

    added file upload and download functionality to Kanon

--- readFile.js
+++ readFile.js
@@ -1,10 +1,37 @@
+const Range = require("ace/range").Range;
+const topRow = __$__.editor.selection.lead.row;
+
+
+
 function readFile() {
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
-
+		document.getElementById('readFile').addEventListener('change', readSingleFile, false);
 	} else {
 		console.error("The File APIs are not fully supported in this browser.")
 	}
 }
 
+function readSingleFile(evt) {
+	//Retrieve the first (and only!) File from the FileList object
+	const f = evt.target.files[0];
+
+	if (f) {
+		const r = new FileReader();
+		r.onload = function(e) {
+			const contents = e.target.result;
+			alert( "Got the file.\n"
+				+"name: " + f.name + "\n"
+				+"type: " + f.type + "\n"
+				+"size: " + f.size + " bytes\n"
+			);
+			__$__.editor.setValue(contents);
+			__$__.editor.selection.clearSelection();
+		};
+		r.readAsText(f);
+
+	} else {
+		alert("Failed to load file");
+	}
+}
 
 
--- saveFile.js
+++ saveFile.js
@@ -1,3 +1,22 @@
 function saveFile(){
+	function download(filename, text) {
+		const element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('download', filename);
 
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	}
+
+// Start file download.
+	document.getElementById("saveButton").addEventListener("click", function(){
+		// Generate download of hello.txt file with some content
+		const content = __$__.editor.getValue();
+		const filename = "savedFile.js";
+		download(filename, content);
+	}, false);
 }
