d42434e48989254ce8663b1981c9511e83dc4c90
Mihir Okte <mihirokte77@gmail.com>
2021-07-14 01:05:09 +0530

    Fixed errors loading error

--- src/js/problem-pane.js
+++ src/js/problem-pane.js
@@ -1,5 +1,4 @@
 var errors_list = "";
-var num_errors = 0;
 
 window.addEventListener('load', 
   function() { 
@@ -11,25 +10,29 @@ __$__.goToLine = function(row,column){
   __$__.editor.getSession().documentToScreenPosition(row,column);
 }
 
-__$__.getErrors = function(e){
-  var annotations = __$__.editor.getSession().getAnnotations();
-
-  for(var i=0; i<annotations.length; i++){
-    errors_list +=
-    "<div>" + annotations[i].type.toUpperCase() + "<div style='color:#ff2424'>" + annotations[i].text + "</div>" + "<button style='background-color:#FFFFFF; border:1px solid #000000;margin-top:4px'> Line No:" + annotations[i].row + " Column No." + annotations[i].column +  "</button> <br></br>";
-    num_errors++;
-  }
-
-  document.getElementById("problem-ball").innerHTML = num_errors;
-  document.getElementById("all-errors").innerHTML = errors_list;
-
-  num_errors = 0;
-  errors_list = "";
+if(__$__.editor){
+  __$__.editor.on('change', function(e){
+    __$__.getErrors(e);
+  },false);
 }
 
-__$__.editor.on('change', function(e){
-  setTimeout(__$__.getErrors(e), 0);
-});
+setInterval(__$__.getErrors,100);
+
+__$__.getErrors = function(){
+
+    var annotations = __$__.editor.getSession().getAnnotations();
+    document.getElementById("problem-ball").innerHTML = annotations.length;
+  
+    for(var i=0; i<annotations.length; i++){
+      errors_list +=
+      "<div>" + annotations[i].type.toUpperCase() + "<div style='color:#ff2424'>" + annotations[i].text + "</div>" + "<button style='background-color:#FFFFFF; border:1px solid #000000;margin-top:4px'> Line No:" + annotations[i].row + " Column No." + annotations[i].column +  "</button> <br></br>";
+    }
+  
+    document.getElementById("all-errors").innerHTML = errors_list;
+    
+    errors_list = "";
+}
+
 
 __$__.openPanel = function() {
     document.getElementById("problem-panel").style.display = "block";
